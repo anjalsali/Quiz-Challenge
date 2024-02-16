242439961a124a8bc934140c28a4eb8fe849ca4b
Anjal Sali <anjalsali55@gmail.com>
2024-02-16 02:17:44 +0000

    added questions file

---
+++ assets/js/questions.js
@@ -0,0 +1,57 @@
+const questions = [
+   {
+      question: "Which type of JavaScript language is ___",
+      choices: ["Object-Oriented", "Object-Based", "Assembly-language", "High-level"],
+      answer: "Object-Based",
+   },
+   {
+      question: "Which one of the following also known as Conditional Expression:",
+      choices: ["Alternative to if-else", "Switch statement", "If-then-else statement", "immediate if"],
+      answer: "immediate if",
+   },
+   {
+      question: "In JavaScript, what is a block of statement?",
+      choices: [
+         "Conditional block",
+         "block that combines a number of statements into a single compound statement",
+         "both conditional block and a single statement",
+         "block that contains a single statement",
+      ],
+      answer: "block that combines a number of statements into a single compound statement",
+   },
+   {
+      question: "When interpreter encounters an empty statements, what it will do:",
+      choices: ["Shows a warning", "Prompts to complete the statement", "Throws an error", "Ignores the statements"],
+      answer: "Ignores the statements",
+   },
+   {
+      question: "The 'function' and 'var' are known as:",
+      choices: ["Keywords", "Data types", "Declaration statements", "Prototypes"],
+      answer: "Declaration statements",
+   },
+   {
+      question: "Which of the following variables takes precedence over the others if the names are the same?",
+      choices: ["Global variable", "The local element", "The two of the above", "None of the above"],
+      answer: "The local element",
+   },
+   {
+      question: "Which one of the following is the correct way for calling the JavaScript code?",
+      choices: ["Preprocessor", "Triggering Event", "RMI", "Function/Method"],
+      answer: "Function/Method",
+   },
+   {
+      question: "Which of the following type of a variable is volatile?",
+      choices: ["Mutable variable", "Dynamic variable", "Volatile variable", "Immutable variable"],
+      answer: "Mutable variable",
+   },
+   {
+      question: "Which of the following option is used as hexadecimal literal beginning?",
+      choices: ["00", "0x", "0X", "Both 0x and 0X"],
+      answer: "Both 0x and 0X",
+   },
+   {
+      question: "In the JavaScript, which one of the following is not considered as an error:",
+      choices: ["Syntax error", "Missing of semicolons", "Division by zero", "Missing of Bracket"],
+      answer: "Division by zero",
+   },
+];
